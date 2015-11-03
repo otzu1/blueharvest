a5aba776189a2a8ad5be257491e71007f6ce82ce
jmb <jamie@whitepumpkin.com>
2015-11-03 00:48:17 -0500

    implement geocache by distance

--- src/blueharvest/geocaching/data/geocache.java
+++ src/blueharvest/geocaching/data/geocache.java
@@ -5,8 +5,6 @@
  */
 package blueharvest.geocaching.data;
 
-import blueharvest.geocaching.webservices.geocache.ArrayOfAnyType;
-
 /**
  *
  * @author jmb
@@ -70,9 +68,9 @@ public class geocache extends blueharvest.geocaching.objects.geocache {
 
     /**
      * <h3>inserts a geocache</h3>
-     * inserts a geocache into the database via web services<br />
+     * inserts a geocache into the database via web services<br>
      * preconditions: the user must exist; location latitude and longitude must
-     * be specified.<br />
+     * be specified.<br>
      * logbook and location inserted alongside
      *
      * @param g (g)eocache
@@ -121,37 +119,98 @@ public class geocache extends blueharvest.geocaching.objects.geocache {
         throw new java.lang.UnsupportedOperationException("Not implemented.");
     }
 
+    /**
+     * <h3>gets geocaches within a radius of center coordinates</h3>
+     * the following are not implemented:
+     * <ul>
+     * <li>geocache.user.anniversary - null</li>
+     * <li>geocache.user.password - null/empty (intentionally)</li>
+     * <li>geocache.user.salt - null/empty (intentionally)</li>
+     * <li>geocache.user.active - true</li>
+     * <li>geocache.user.locked - false</li>
+     * <li>geocache.user.image - null</li>
+     * <li>geocache.user.location - null</li>
+     * <li>geocache.images - null (todo)</li>
+     * <li>geocache.location.name - empty</li>
+     * <li>geocache.location.address - null</li>
+     * <li>geocache.logbook.entries - null (todo)</li>
+     * </ul>
+     * @param minlatrad minimum latitude in radians
+     * @param maxlatrad maximum latitude in radians
+     * @param minlngrad minimum longitude in radians
+     * @param maxlngrad maximum longitude in radians
+     * @param latrad center latitude in radians
+     * @param lngrad center longitude in radians
+     * @param distance measurement from center coordinates
+     * @return a list of geocaches within the distance from the center
+     *         coordinates
+     * @since 2015-11-02
+     * @see <a href="http://JanMatuschek.de/LatitudeLongitudeBoundingCoordinates#Java">
+     * http://JanMatuschek.de/LatitudeLongitudeBoundingCoordinates#Java</a>
+     * @see blueharvest.geocaching.util.GeoLocation
+     */
     public static java.util.ArrayList<geocache> getWithinDistance(
         double minlatrad, double maxlatrad, double minlngrad, double maxlngrad,
         double latrad, double lngrad, double distance) {
-        java.util.ArrayList<blueharvest.geocaching.objects.geocache> g
-            = new java.util.ArrayList<>();
-        java.util.List<Object> f = getGeocachesWithinDistance(
+        java.util.ArrayList<geocache> g = new java.util.ArrayList<>();
+        java.util.List<Object> h = getGeocachesWithinDistance(
             minlatrad, maxlatrad, minlngrad, maxlngrad,
             latrad, lngrad, distance).getAnyType();
         // get the geocaches from one array to another
-        if (f.size() > 0) {
-            for (int i = 0; i < f.size(); i++) { // todo: enhanced loop
-                // START HERE
-                //f.get(i);
+        if (h.size() > 0) {
+            for (int i = 0; i < h.size(); i++) { // todo: enhanced loop
+                // or new geocache(...)
+                g.add(create(
+                    java.util.UUID.fromString(
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getId()),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getAnniversary().toGregorianCalendar().getTime(),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getName(),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getDescription(),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getDifficulty(),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getSize(),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getTerrain(),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getStatus(),
+                    ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getType(),
+                    blueharvest.geocaching.data.user.create(java.util.UUID.fromString(
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getUser().getId()),
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getUser().getAnniversary().toGregorianCalendar().getTime(), // not implemented
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getUser().getUsername(),
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getUser().getPassword(), // intentionally not implemented
+                        java.util.UUID.fromString(((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getUser().getSalt()), // intentionally not implemented
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getUser().getEmail(),
+                        true, false, null, null), // active, locked, image, and location not implemented 
+                    null, // java.util.ArrayList<blueharvest.geocaching.objects.image> images not implemented, todo
+                    blueharvest.geocaching.data.location.create(
+                        java.util.UUID.fromString(
+                            ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getLocation().getId()),
+                        null, // location.name not implemented
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getLocation().getLatitude(),
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getLocation().getLongitude(),
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getLocation().getAltitude(),
+                        null), // location.address not implemented
+                    blueharvest.geocaching.data.logbook.create(java.util.UUID.fromString(
+                        ((blueharvest.geocaching.webservices.geocache.Geocache) h.get(i)).getLogbook().getId()),
+                        null))); // logbook.entries not implemented
             }
         }
-return null; // todo
+        return g;
     }
 
-    private static ArrayOfAnyType getGeocachesWithinDistance(
-        double minlatrad, double maxlatrad, double minlngrad, double maxlngrad,
-        double latrad, double lngrad, double distance) {
-        try {
-            return getServicePort().getGeocachesWithinDistance(
-                minlatrad, maxlatrad, minlngrad, maxlngrad, latrad,
-                lngrad, distance, getServiceCredentials());
-        } catch (java.lang.Exception ex) { // java.net.ConnectException
-            java.util.logging.Logger.getLogger(
-                new Object().getClass().getName()).log(
-                    java.util.logging.Level.SEVERE, null, ex);
-            return null;
+    private static blueharvest.geocaching.webservices.geocache.ArrayOfAnyType
+        getGeocachesWithinDistance(
+            double minlatrad, double maxlatrad, double minlngrad,
+            double maxlngrad, double latrad, double lngrad, double distance) {
+            try {
+                return getServicePort().getGeocachesWithinDistance(
+                    minlatrad, maxlatrad, minlngrad, maxlngrad, latrad,
+                    lngrad, distance, getServiceCredentials());
+            } catch (java.lang.Exception ex) { // java.net.ConnectException
+                java.util.logging.Logger.getLogger(
+                    new Object().getClass().getName()).log(
+                        java.util.logging.Level.SEVERE, null, ex);
+                System.out.println(ex.getMessage());
+                return null;
+            }
         }
-    }
 
 }
